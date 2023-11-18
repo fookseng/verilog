312d4b95bee5c714574673f9354f305396771e4c
fookseng <50125285+fookseng@users.noreply.github.com>
2023-11-18 22:44:14 +0800

    Add files via upload

--- synopsys_directive.v
+++ synopsys_directive.v
@@ -3,31 +3,43 @@ module synopsys_directive
 ( 
 input				clk,
 input				reset, 
-input		[1:0]	sel, 
+input		[3:0]	sel, 
 input   	[7:0]	data, 
-output reg 			finish
+output  			finish
 );
+
+reg 		[1:0]  	temp;
+
+assign finish = &temp;
 
 always@(posedge clk, negedge reset) begin
 	if(!reset) begin
-		finish <= 1'b0;
+		temp <= 2'b0;
 	end
 	else begin
-		case(sel) // synopsys parallel_case
-			2'b00: begin
-				finish <= &data;
+		case(sel) // synopsys parallel_case // synopsys full_case
+			4'b0001: begin
+				temp <= data[1:0];
 			end
-			2'b01: begin
-				finish <= |data;
+			4'b0010: begin
+				temp <= data[3:2];
 			end
-			2'b10: begin
-				finish <= ~&data;
+			4'b0100: begin
+				temp <= data[5:4];
 			end
-			default: begin
-				finish <= ~|data;
+			4'b1000: begin
+				temp <= data[7:6];
 			end
 		endcase
+
+		/*
+		temp <= {2{sel[0]}} & data[1:0] |
+				{2{sel[1]}} & data[3:2] |
+				{2{sel[2]}} & data[5:4] |
+				{2{sel[3]}} & data[7:6] ;
+		*/
 	end
 end
+
 endmodule
 
